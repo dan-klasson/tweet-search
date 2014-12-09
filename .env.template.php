8129c0cdeefcac3dcee77fc1c44e0a3f18b8f694
Daniel Klasson <dan.klasson@gmail.com>
2014-12-09 21:29:27 +0700

    added environment template

---
+++ .env.template.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'DEFAULT_ADDRESS' => 'bangkok',
+    'TWITTER_ACCESS_TOKEN' => '',
+    'TWITTER_TOKEN_SECRET' => '',
+    'GOOGLE_GEO_API_URI' => '',
+    'GOOGLE_MAPS_API_KEY' => '',
+);
+
