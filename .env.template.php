96a3871309b0c4833396dad6f3140860dfb19da6
Daniel Klasson <dan.klasson@gmail.com>
2014-12-23 17:14:22 +0700

    updated the env tmp file

--- .env.template.php
+++ .env.template.php
@@ -2,9 +2,19 @@
 
 return array(
     'DEFAULT_ADDRESS' => 'bangkok',
-    'TWITTER_ACCESS_TOKEN' => '',
-    'TWITTER_TOKEN_SECRET' => '',
     'GOOGLE_GEO_API_URI' => '',
     'GOOGLE_MAPS_API_KEY' => '',
+    'TWITTER_ACCESS_TOKEN' => '',
+    'TWITTER_TOKEN_SECRET' => '',
+    'TWITTER_API_KEY'		=> '',
+    'TWITTER_API_SECRET'	=> '',
+	'TWITTER_RESULT_TYPE'   => 'recent',
+	'TWITTER_RESULT_AMOUNT' => 50,
+	'TWITTER_RESULT_DISTANCE' => 50,
+	'TWITTER_RESULT_UNIT'	=> 'km',
+	'DB_HOST'				=> 'localhost',
+	'DB_USER'				=> 'root',
+	'DB_NAME'				=> 'tweet',
+	'DB_PWD'				=> '',
 );
 
